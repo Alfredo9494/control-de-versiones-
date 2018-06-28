74b34e8a5e0fee5103d52177b736cecdc401360f
anibal camacho notario <avenocturna_96@hotmail.com>
2018-06-27 19:14:54 -0500

    se creo el diseño de una calculadora

--- control de verciones/Form1.cs
+++ control de verciones/Form1.cs
@@ -16,5 +16,10 @@ namespace control_de_verciones
         {
             InitializeComponent();
         }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "0";
+        }
     }
 }
